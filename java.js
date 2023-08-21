605f54b83533aa11d812aab65d75838790b957ca
tarik-Darklord <142804471+tarik-Darklord@users.noreply.github.com>
2023-08-21 14:02:44 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+// scrol
+window.onscroll = () => { 
+
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+}
